7ad5afbeba335c4f96179851a9e0819b529ca5d2
Damian Nowak <Foxnacity@gmail.com>
2025-03-11 09:25:54 +0100

    feat: add tabs to sidebar

--- resources/js/Layouts/Components/Sidebar.tsx
+++ resources/js/Layouts/Components/Sidebar.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Drawer, Stack, Tab, Tabs, Typography, useMediaQuery, useTheme } from '@mui/material';
 
 interface SidebarProps {
   open: boolean;
 }
 
+enum SidebarTabs {
+  Groups,
+  Teachers,
+  Classes
+}
+
 const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
   const theme = useTheme();
   const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const [activeTab, setActiveTab] = useState<SidebarTabs>(SidebarTabs.Groups);
+
+  const tabStyles = {
+    fontWeight: 'bold',
+    color: '#c1c1c1',
+    '&.Mui-selected': { color: theme.palette.primary.contrastText }
+  };
+
+  const handleTabChange = (event: React.SyntheticEvent, value: number) => {
+    setActiveTab(value);
+  };
+
   return (
     <Drawer
       open={props.open}
@@ -23,20 +41,45 @@ const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
         },
       }}
     >
-      <Stack height={'100%'}
+      <Stack
+        alignItems={'center'}
+        height={'100%'}
         justifyContent={'space-between'}>
         <Stack alignItems={'center'}
-          gap={1}>
+          gap={0.5}>
           <Typography textAlign={'center'}
-            variant='h1'>
-            Wydział nauk ścisłych i technicznych
+            variant='h3'>
+            Wydział nauk ścisłych i&nbsp;technicznych
           </Typography>
-          <Typography variant='h2'>Uniwersytet Śląski</Typography>
+          <Typography variant='h4'>Uniwersytet Śląski</Typography>
+          <Typography variant='h5'>2024/2025</Typography>
+          <Typography variant='body1'>semestr letni</Typography>
         </Stack>
-        <Box>
-          <Tabs>
-            <Tab></Tab>
-          </Tabs>
+        <Box flexGrow={5}>
+          <Stack
+            direction={'row'}>
+            <Tabs
+              indicatorColor='secondary'
+              value={activeTab}
+              onChange={handleTabChange}
+            >
+              <Tab
+                label='Grupy'
+                sx={tabStyles}
+                value={SidebarTabs.Groups}
+              />
+              <Tab
+                label={'Nauczyciele'}
+                sx={tabStyles}
+                value={SidebarTabs.Teachers}
+              />
+              <Tab
+                label='Sale'
+                sx={tabStyles}
+                value={SidebarTabs.Classes}
+              />
+            </Tabs>
+          </Stack>
         </Box>
         <Stack alignItems={'center'}>
           <Typography>Obsługa rezerwacji</Typography>
